637bf2d25592efad60c41989fab9b911923afa24
christopher-sanchez <csanchez@my.okcu.edu>
2026-04-23 14:05:35 -0500

    set key and values

--- Hash/hash.js
+++ Hash/hash.js
@@ -4,8 +4,9 @@ constructor(initalCapacity=16,loadFactor = 0.75 ){
     this.loadFactor = loadFactor;
     this.buckets = new Array(this.capacity).fill(null).map(()=>[]);
     this.size = 0;
-    
+
 }
+
  hash(key) {
   let hashCode = 0;
 
@@ -16,6 +17,23 @@ constructor(initalCapacity=16,loadFactor = 0.75 ){
 
   return hashCode;
 } 
+set(key,value){
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+    
+    for(let pair of bucket){
+        if(pair[0]=== key){
+            pair[1] = value;
+            return;
+        }
+    }
+    bucket.push([key,value]);
+    this.size++;
+    if(this.size / this.capacity > this.loadFactor){
+        this.resize();
+    }
+}
+
 
 
 }
