a2274166fa84dffd4ffab4081054b970dd5826ec
christopher-sanchez <csanchez@my.okcu.edu>
2026-04-23 15:11:41 -0500

    added test and debugged

--- Hash/hash.js
+++ Hash/hash.js
@@ -1,8 +1,8 @@
-class HashMap {
+export class HashMap {
 constructor(initalCapacity=16,loadFactor = 0.75 ){
     this.capacity = initalCapacity;
     this.loadFactor = loadFactor;
-    this.buckets = new Array(this.capacity).fill(null).map(()=>[]);
+    this.buckets = new Array(this.capacity).fill(null).map(() => []);
     this.size = 0;
 
 }
@@ -15,7 +15,7 @@ constructor(initalCapacity=16,loadFactor = 0.75 ){
     hashCode = primeNumber * hashCode + key.charCodeAt(i);
   }
 
-  return hashCode;
+  return hashCode % this.capacity;
 } 
 set(key,value){
     const index = this.hash(key);
@@ -30,11 +30,40 @@ set(key,value){
     bucket.push([key,value]);
     this.size++;
     if(this.size / this.capacity > this.loadFactor){
-        this.resize();
+        this._resize();
     }
 }
+get(key){
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+
+    for(let pair of bucket){
+        if(pair[0] === key) return pair[1];
+    }
+    return null;
+}
+has(key){
+    return this.get(key) !== null;
+}
+
 
 
 
 }
+
+const test = new HashMap() 
+
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+
 
