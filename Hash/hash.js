23a0e8d6b7d63ef76cfb0fd95a990ce0431cef59
christopher-sanchez <csanchez@my.okcu.edu>
2026-04-23 13:54:47 -0500

    refactoring

--- Hash/hash.js
+++ Hash/hash.js
@@ -1,8 +1,12 @@
-function HashMap(){
-
+class HashMap {
+constructor(initalCapacity=16,loadFactor = 0.75 ){
+    this.capacity = initalCapacity;
+    this.loadFactor = loadFactor;
+    this.buckets = new Array(this.capacity).fill(null).map(()=>[]);
+    this.size = 0;
+    
 }
-
-function hash(key) {
+ hash(key) {
   let hashCode = 0;
 
   const primeNumber = 31;
@@ -12,3 +16,7 @@ function hash(key) {
 
   return hashCode;
 } 
+
+
+}
+
